22099f7251f159ee068be93718c0c9185b84820b
theSaneHatter <trollroy.com@gmail.com>
2025-04-27 17:04:08 -0700

    removed hella bloat and make code more suitable for create task

--- lib.py
+++ lib.py
@@ -1,263 +1,8 @@
 import time
-from PIL import Image, ImageDraw
-import PIL
-import os
+from PIL import Image
 import numpy as np
-import math
 
-
-def outline(point, image, width=50, thickness="1 (its an int tho)", color="red"):
-    if thickness == "1 (its an int tho)":
-        thickness=round((image.size[0] + image.size[1])/200)
-
-    draw = ImageDraw.Draw(image)
-    if point[0]-width/2 < 0 or point[1]-width/2 < 0:
-
-        print('\033[31mWorning from function >outline()<: some of your box might be outta the frame\033[0m')
-    shape = [
-        (point[0]-width/2,point[1]-width/2),
-        (point[0]+width/2, point[1]+width/2)
-    ]
-    draw.rectangle(shape,outline=color,width=thickness)
-
-
-def compare(a,b,dif):
-    if len(a) != len(b):
-        return IndexError
-    c = 0
-    for n in range(len(a) -1):
-        if type(c) == int:
-            c = abs(a[n] - b[n])
-            if c > dif:
-                return False
-    return True
-
-
-#returns avg rgb of image
-def find_avg(image):
-    #note: could be acheved by image.resize(1,1)
-    image = image.convert("RGB")
-    pixels = np.array(image)
-    r = np.mean(pixels[:,:,0])
-    g = np.mean(pixels[:,:,1])
-    b = np.mean(pixels[:,:,2])
-    r = round(r)
-    g = round(g)
-    b = round(b)
-    e = (r,g,b)
-    return e
-
-
-def sort_by_brightness(image):
-    pixels = np.array(image)
-    pixels_1d = pixels.reshape(-1, 3)
-    brightness = np.sum(pixels_1d, axis=1)
-    indices = np.argsort(brightness)
-    sorted_pixels = pixels_1d[indices]
-    sorted_image_array = sorted_pixels.reshape(pixels.shape)
-    sorted_image = Image.fromarray(sorted_image_array)
-    return sorted_image
-
-
-#r,g,b,h,w,0 
-#takes np list of pixels, r,g,b, and manipulates their list so that 
-#the 6th value in the rgb list is the frequency of the r,g,b
-def pixel_list_mode_old(list_arg, dimen=1):
-    l = list_arg.copy()
-
-    biggests = []
-    #clear frequency index
-    l[:,:,5] = 0
-
-    for i in range(dimen):
-        biggest = [[0,0,0],0]
-        biggests.append(biggest)
-
-        for y in l:
-            for x in y:
-                for y1 in l:
-                    for x1 in y1:
-                        if np.array_equal(x[:2],x1[0:2]) and (x[5] != x1[5] or x[5] == x1[5] == 0):
-                            x[5] += 1
-                            
-        biggest = [[0,0,0],0]
-        biggests.append(biggest)
-        for y in l:
-            for x in y:
-                if  x[5] > biggests[i][1]:
-                    biggest[0][:3] = x[:3]
-                    biggest[1] = x[5]
-        biggests.append(biggest)
-        #clear every freq except highest unless on last run
-        if i != dimen -1:
-            for j in l[0]:
-                if biggests[i][1] != j[5]:
-                    j[i]
-    return biggests
-
-
-#takes pixels_with_location, r,g,b,h,w,0
-#takes np list of pixels, r,g,b, and returns a list of the frequency of the
-# r,g,b so that the 6th index (arr[5]) is the frequency, with the pixels sorted 
-#all duplacates are removed.
-def pixel_list_mode(pixels_with_location,dimen=None, mode_given=False):
-    if not mode_given:
-        array = give_pixels_mode(pixels_with_location)
-
-    # cleaned_array = cleaned_array.reshape(array.shape)    
-    # array = np.unique(array, axis=0)
-    _, index = np.unique(array[:, :3], axis=0, return_index=True)
-    array = array[index]
-
-    array = array[np.argsort(array[:, 5])]
-    array = array[::-1]
-
-    if dimen != None:
-        return array[:dimen, :]
-    else:
-        return array
-
-
-#takes pixels_with_location, r,g,b,h,w,0
-#takes np list of pixels, r,g,b, and manipulates their list so that 
-#the 6th value in the rgb list is the frequency of the r,g,b
-def give_pixels_mode(pixels_with_location,dimen=None):
-    array = pixels_with_location.copy()
-    array = array.reshape(-1,6)
-    array[:,5] = 0
-    array = array[np.argsort(array[:,5])]
-    array = array[::-1]
-    for j in array:
-        for k in array:
-            if np.array_equal(j[:3], k[:3]):  # changed from j[:2] to j[:3]
-                j[5] += 1
-
-    array = array[np.argsort(array[:, 5])]
-    array = array[::-1]
-
-    if dimen != None:
-        return array[:dimen, :]
-    else:
-        return array
-
-def give_pixels_location(pixels, remove=False):
-    if remove == False:
-        shape = [0]*(pixels.shape[0] * pixels.shape[1] * (pixels.shape[2]+3))
-        pixels_with_location = np.array(shape)
-        
-        shape = pixels.shape[0],pixels.shape[1],(pixels.shape[2] + 3 )
-        pixels_with_location = pixels_with_location.reshape(shape[0],shape[1],shape[2])
-
-        for a,y in enumerate(pixels_with_location):
-            for b,x in enumerate(y):
-                pixels_with_location[a,b,0] = pixels[a,b,0]
-                pixels_with_location[a,b,1] = pixels[a,b,1]
-                pixels_with_location[a,b,2] = pixels[a,b,2]
-                pixels_with_location[a,b,3] = a
-                pixels_with_location[a,b,4] = b
-
-        return pixels_with_location
-    
-    if remove:
-        shape = pixels.shape[0], pixels.shape[1], 3
-        #('shape var: ', shapeprint)
-        pixels_w_o_location = np.zeros(math.prod(shape), dtype=np.uint8)
-        pixels_w_o_location = pixels_w_o_location.reshape(pixels.shape[0], pixels.shape[1], 3)
-        pixels_w_o_location[:,:,:] = pixels[:,:,:3]
-        return pixels_w_o_location
-
-
-#r,g,b,h,w,occurance
-def blackout_white(pixles_w_loc,dif=10, mode=None):
-    print('pixles_w_locs.shape',pixles_w_loc.shape)
-    print('pixel_list_mode(pixles_w_loc, dimen=3): ', pixel_list_mode(pixles_w_loc, dimen=1))
-    if mode==None:
-        mode = pixel_list_mode(pixles_w_loc, dimen=1)[0]
-    print('mode: ', mode)
-    l = pixles_w_loc.copy()
-    print('l.shape:',l.shape)
-    for j in l:
-        for k in j:
-            if compare(k[:3], mode, dif):
-                k[:3] = [0]*3
-    return l
-   
-        
-def benchmark(fn,arguements=None):
-    if arguements==None:
-        start_time = time.time()
-        fn()
-        elapsed = time.time() - start_time
-        return elapsed
-    else:
-        start_time = time.time()
-        fn(*arguements)
-        elapsed = time.time() - start_time
-        return elapsed
-
-
-#returns mean dif of 2 lists
-def mean_dif(a,b):
-    if len(a) != len(b):
-        print(f"\033[31mError in mean_dif():\n   len({a}) != len({b}).\narg a & arg b must have = length")
-    difs = []
-    for i in range(len(a)):
-        dif = abs(a[i] - b[i])
-        difs.append(dif)
-    mean = sum(difs)/len(difs)
-    return mean
-
-
-#takes [[r,g,b,h,w,occurances]] 
-#sorts by r,g,b
-def sort_by_color(pixels_with_loc):
-    # calculate similarity btwn each pixel and the first pixel
-    array = pixels_with_loc.reshape(-1,6).copy()
-    similarities = [mean_dif(pixel[:3], array[0, :3]) for pixel in array]
-
-    #sort pixels based on similarity
-    sorted_indices = np.argsort(similarities)
-    sorted_array = array[sorted_indices]
-    sorted_array = sorted_array.reshape(pixels_with_loc.shape)
-    #print(sorted_array)
-    return sorted_array
-
-
-#takes r,g,b,h,w,0 list genorated by give_pixels_location() and pixel_list_mode
-#returns pixels sorted based on their mode index
-def sort_pixels_by_mode(pixels_with_loc, modes_given=False):
-    # sorted_array = pixels_with_loc.copy()
-    # if not modes_given:
-    #     sorted_array = give_pixels_mode(sorted_array)
-    
-    # sorted_array = sorted_array.reshape(-1,6)
-    # sorted_array = sorted_array[np.argsort(sorted_array[:,5])]
-    # sorted_array = sorted_array[::-1]
-    # sorted_array = sorted_array.reshape(pixels_with_loc.shape)
-    array = pixels_with_loc.copy()
-    mode = pixel_list_mode(array.copy())
-    #print('mode:',mode)
-    array =  array.reshape(-1,6)
-    sorted_array = array.copy()
-    sorted_array[:,:] = 0
-    count = 0
-
-    total_pixels = sum(mode[:,5])
-    #print("total_pixels:", total_pixels)
-    sorted_array = np.zeros((total_pixels,6), dtype=int)
-    sorted_array = sorted_array.reshape(total_pixels,6)
-
-    for j in mode:
-        sorted_array[count:count+j[5],:] = j
-        count += j[5]
-    
-    #print('pixels_with_loc.shape: ',pixels_with_loc.shape)
-    #print('sorted_array.shape: ',sorted_array.shape)
-    #print()
-    sorted_array = sorted_array.reshape(pixels_with_loc.shape)
-    return sorted_array
-
-# trims blank lines from array
+#trims blank lines from array
 def trim_blank(arr, r2=0.01):
     bitmask = np.add.reduce(arr,1) < arr.shape[1] - arr.shape[1]*r2
     bitmask_f = np.logical_or.accumulate(bitmask)
@@ -283,36 +28,8 @@ def edge_detect(img,buckets=2):
     pixels_l, pixels_up = np.roll(pixels,1,1), np.roll(pixels,1,0)
     bitmap_l, bitmap_up = np.equal(pixels_l, pixels), np.equal(pixels_up, pixels)
     bitmap = np.logical_and(bitmap_l, bitmap_up)
-    # bitmap = trim_blank(bitmap)
-    # bitmap = np.transpose(trim_blank(np.transpose(bitmap)))
-    # bitmap = np.multiply(bitmap, 255)
+
     return bitmap
-
-
-def get_neighbors(bitmap):
-    if np.sum(bitmap) <= 0:
-        print('\033[32mWorning from GOL_clock(): Entered array are all zeros\033[0m')
-        return None
-    rows, colms = bitmap.shape
-    bitmap_save = np.zeros((rows+2,colms+2),dtype=int)
-    bitmap_save[1:rows+1,1:colms+1] = bitmap[:,:]
-    
-
-    shift_up = np.roll(bitmap_save,1,0)
-    shift_down = np.roll(bitmap_save,-1,0)
-    shift_right = np.roll(bitmap_save,1,1)
-    shift_left = np.roll(bitmap_save,-1,1)
-
-    shift_RU = np.roll(shift_right,1,0)
-    shift_RD = np.roll(shift_right,-1,0)
-    shift_LU = np.roll(shift_left,1,0)
-    shift_LD = np.roll(shift_left,-1,0)
-    
-    neighbers = shift_up+shift_down+shift_right+shift_left+shift_RU+shift_RD+shift_LU+shift_LD
-    
-    neighbers = neighbers[1:rows, 1:colms]
-    
-    return neighbers
 
 #takes bitmap shape: (n,n)
 #returns list with each true value uneque number shape: (n,n)
@@ -320,20 +37,6 @@ def get_neighbors(bitmap):
 def trues_unique_int(arr_arg):
     arr = arr_arg.copy()
     arr[arr==1] = np.arange(1,np.sum(arr[arr==1].shape) +1 )
-    return arr
-
-#takes bitmap shape: (n,n)
-#returns list with each true value uneque number shape: (n,n)
-    #with each true value uneque number
-#currently not the fastest, but orders the numbers so they are the smallest possable
-def trues_uneque_int_lineor(arr_arg):
-    arr = arr_arg.copy()
-    n = 1
-    for colomn in range(arr.shape[0]):
-        for element in range(arr.shape[1]):
-            if arr[colomn,element] != 0:
-                arr[colomn,element] = n
-                n+=1
     return arr
 
 #takes bitmap, returns list of that bitmap shifted in all 9ds (9th being same thingy)
@@ -417,7 +120,6 @@ def lines_unique_int(arr_arg):
 
     return arr
             
-
 #gives avg location of 1s in bitmask
 #takes bitmask, returns avg (x,y) posistion
 def avg_location(arr_arg):
@@ -427,7 +129,6 @@ def avg_location(arr_arg):
     avg = avg_x,avg_y
     return avg
 
-
 #takes bitmask, returns avg (x,y) posistion
 #gives avg location of 1s in bitmask
 def center(arr_arg):
@@ -436,7 +137,6 @@ def center(arr_arg):
     avg_y = np.average(locs[1])
     avg = avg_x,avg_y
     return avg
-
 
 #takes bitmask, returns the std of the distance of each True point to the avg point
 def distance_std(bitmask):
@@ -510,6 +210,7 @@ def rank_shapes_as_circles(arr_arg, depth=None):
     # ranks = ranks[::-1]
     return ranks
 
+# takes image path and returns the number of pips it sees
 def count_pips(image_path, buckets=4,removal_size=50,std_bar=1,time_myself=False, show_processing=True, resizement=5):
     start = time.time()
 
@@ -538,7 +239,8 @@ def count_pips(image_path, buckets=4,removal_size=50,std_bar=1,time_myself=False
         Dice.show()
     return pips_count
 
-
+#communacates with the user to process dice image. 
+#
 def PR():
     print(f'\033[4mDice Recognizer\033[0m')
     dice_picture_path = input("Enter Picture of dice path:")
--- main.py
+++ main.py
@@ -1,75 +1,19 @@
-from PIL import Image, ImageDraw, ImageEnhance
+from PIL import Image
 from lib import *
 import numpy as np
+
 start = time.time()
-#purple is ~90,0,70
-#green ~90,115,0
+
 
 #so bools dont look wierd
 np.set_printoptions(formatter={'float': '{:0.2f}'.format})
 
 path = './assets/Dice1.jpg'
-# path = './assets/Dice2.jpg'
-
-Dice = Image.open(path) 
-
-#sharpness 
-sharpness = ImageEnhance.Sharpness(Dice)
-Dice = sharpness.enhance(1)
-
-#contrast 
-contrast = ImageEnhance.Contrast(Dice)
-Dice = contrast.enhance(1)
 
 
-#brightness
-brightness = ImageEnhance.Brightness(Dice)
-Dice = brightness.enhance(1)
-
-
-# resize 
-print(Dice.size)
-h,w = Dice.size
-d = 5
-h,w = round(h/d),round(w/d)
-print('new size', h,w)
-size = (h,w)
-Dice = Dice.resize(size)
-
-# Dice.show()
-
-
-'''
-pixels = edge_detect(Dice,buckets=4)
-
-
-
-# pixels = trim_all_blanks(pixels)
-
-
-pixels = lines_unique_int(~pixels)
- 
-removel_size = 50 
-pixels = remove_small_lines(pixels,removel_size,blank_id=1)
-
-Dice = Image.fromarray(pixels)
-Dice.show()
-
-ranked = rank_shapes_as_circles(pixels)
-std_bar = 1
-ranked = ranked[ranked[:,1] < std_bar]
-pixels[~np.isin(pixels,ranked[:,0])] = 0
-
-print(f'\033[32mAnd the number on the dice is: {np.size(np.unique(pixels)) -1}\033[0m')
-
-
-# pixels = np.multiply(pixels, 255)
-Dice = Image.fromarray(pixels)
-# Dice = Dice.resize()
-Dice.show()
-'''
-print(print(PR()))
-
+# UNCOMMENT FOR SUBMISSION
+#print(print(PR()))
+print(count_pips(path, buckets=4, removal_size=50, std_bar=1, time_myself=True, show_processing=True, resizement=5))
 
 total_time = time.time() - start
 print(f'\033[32mProcess finished.\nElapsed time: {round(total_time,5)} secends.\033[0m')
